60a90e09e8340b90f39584f70bca472c23759c4f
teachgame <teachgame@163.com>
2019-03-13 15:34:12 +0800

    HelloGame - 游戏循环

---
+++ HelloGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            game.gameName = "王氏自走棋";
+
+            game.Start();
+
+            while(game.isRunning)
+            {
+                game.Update();
+            }
+
+            game.End();
+        }
+    }
+}
