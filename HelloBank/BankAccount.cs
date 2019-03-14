89d1f0a79176268719ad7ee39c0c921108b778b7
teachgame <teachgame@163.com>
2019-03-14 12:11:10 +0800

    Day02-HelloBank

---
+++ HelloBank/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloBank
+{
+    class BankAccount
+    {
+        private string accountName; // 账户名
+        private int age;            // 年龄
+        private int amount;         // 账户余额
+
+        // 构造函数，用于创建新的银行账户对象实例，var_name：账户名，var_age：年龄，var_amount：初始余额
+        public BankAccount(string var_name, int var_age, int var_amount)
+        {
+            accountName = var_name;
+            age = var_age;
+            amount = var_amount;
+
+            Console.WriteLine("您的新账号：" + accountName + "," + age + "," + amount);
+        }
+
+        // 存款方法
+        public void Deposite(int deposite_amount)
+        {
+            int new_amount = amount + deposite_amount;
+            amount = new_amount;
+            Console.WriteLine("您存入" + deposite_amount + "，新的余额是：" + amount);
+        }
+
+        // 取款方法
+        public void WithDraw(int withdraw_amount)
+        {
+            if(withdraw_amount <= amount)
+            {
+                int new_amount = amount - withdraw_amount;
+                amount = new_amount;
+                Console.WriteLine("您取出" + withdraw_amount + "，新的余额是：" + amount);
+            }
+            else
+            {
+                Console.WriteLine("余额不足");
+            }
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -17,13 +17,69 @@ namespace HelloWorld
         private static bool isMale;             // “用户是男的”布尔值变量
         private static bool isCorrectSexType;   // “性别类型是否正确”布尔值变量
 
+        private static string ageString;
+        private static int age;
+
+        private static string paiString;
+        private static float pai;
+
+        private static int balance;
+
+        private static int int_num = 2;             // 整型变量用来存放整数
+        private static float float_num = 3.14f;     // 浮点变量用来存放带小数的数
+
         static void Main(string[] args)
-        {
+        {            
+            SayHello();
+
+            Add(1,2);
+
+            Console.WriteLine("本金加利息总共是：" + GetBalance(10000, 1500));
+
+            balance = GetBalance(2000, 100);
+
+            Console.WriteLine("balance:" + balance);
+
+            Console.WriteLine(int_num);
+            Console.WriteLine(float_num);
+
             Console.WriteLine("您好，请输入姓名：");
             userName = Console.ReadLine();
 
             Console.WriteLine("请输入性别：");
             sex = Console.ReadLine();
+            
+            Console.WriteLine("请输入年龄：");
+            ageString = Console.ReadLine();
+
+            if(int.TryParse(ageString, out age))
+            {
+                Console.WriteLine("您今年" + age + "岁");
+            }
+            else
+            {
+                Console.WriteLine("年龄请输入整数！");
+            }
+
+            Console.WriteLine("请回答圆周率（精确到小数点后两位）：");
+            paiString = Console.ReadLine();
+
+            if(float.TryParse(paiString, out pai))
+            {
+                if(pai == 3.14f)
+                {
+                    Console.WriteLine("回答正确");
+                }
+                else                    
+                {
+                    Console.WriteLine("回答错误！");
+                }
+            }
+            else
+            {
+                Console.WriteLine("请输入浮点数！");
+            }
+
 
             isCorrectSexType = (sex == "男") || (sex == "女");    // sex字符串的内容是“男”或者“女”，性别类型就是正确的
 
@@ -56,6 +112,21 @@ namespace HelloWorld
             {
                 Console.WriteLine("性别类型:" + sex + "，不正确");
             }
+        }
+
+        private static void SayHello()
+        {
+            Console.WriteLine("在SayHello方法里面说Hello");
+        }
+
+        private static void Add(int num1, int num2)
+        {
+            Console.WriteLine(num1 + "+" + num2 + "=" + (num1 + num2));
+        }
+
+        private static int GetBalance(int amount, int extra)
+        {
+            return (amount + extra);
         }
     }
 }
