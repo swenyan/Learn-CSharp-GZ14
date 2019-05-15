413b31369d3d4800b2eddd96ca2d85a3e2a7bfcb
teachgame <teachgame@163.com>
2019-05-15 10:01:41 +0800

    Dictionary

---
+++ HelloDictionary/Human.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloDictionary
+{
+    class Human
+    {
+        public string name;
+        public int age;
+
+        public Human(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void ShowProfile()
+        {
+            Console.WriteLine("name:{0}, age:{1}", name, age);
+        }
+    }
+}
---
+++ HelloDictionary/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] english = new string[] { "sun", "moon", "earth" };
+            string[] chinese = new string[] { "太阳", "月亮", "地球" };
+
+            Dictionary<Human, bool> isCnDict = new Dictionary<Human, bool>();
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            Human cn = new Human("WAWA", 14);
+            Human us = new Human("BABY", 6);
+
+            isCnDict.Add(cn, true);
+            isCnDict.Add(us, false);
+
+            dict.Add("sun", "太阳");
+            dict.Add("moon", "月亮");
+            dict.Add("earth", "地球");
+
+            foreach( bool b in isCnDict.Values )
+            {
+                Console.WriteLine("CN:" + b);
+            }
+
+            string enword = "moon";
+            string cnword = CheckDictionary(enword, english, chinese);
+
+            Console.WriteLine(enword + " is " + cnword + " in Chinese.");
+
+            Console.WriteLine(dict["sun"]);
+            Console.WriteLine(dict["moon"]);
+            Console.WriteLine(dict["earth"]);
+
+            Console.ReadKey();
+        }
+
+        static string CheckDictionary(string word, string[] english, string[] chinese)
+        {
+            int wordIndex = -1;
+
+            for(int i = 0; i < english.Length; i++)
+            {
+                if(word == english[i])
+                {
+                    wordIndex = i;
+                    break;
+                }
+            }
+
+            if(wordIndex == -1)
+            {
+                return null;
+            }
+            else
+            {
+                return chinese[wordIndex];
+            }
+        }
+    }
+}
